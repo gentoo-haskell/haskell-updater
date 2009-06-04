8423cbbe38bba6c2c676c0bf58fa77554414dc82
Ivan.Miljenovic <Ivan.Miljenovic@gmail.com>
2009-06-04 21:59:07 +0000

    Add support to get GHC's version from Cabal Not sure if we should keep this or get the output of "ghc --version"

--- Distribution/Gentoo/GHC.hs
+++ Distribution/Gentoo/GHC.hs
@@ -3,12 +3,14 @@ module Distribution.Gentoo.GHC where
 import Distribution.Simple.PackageIndex(brokenPackages)
 import Distribution.Simple.GHC(getInstalledPackages,configure)
 import Distribution.Simple.Program(defaultProgramConfiguration)
-import Distribution.Simple.Compiler(PackageDB(GlobalPackageDB))
+import Distribution.Simple.Compiler(PackageDB(GlobalPackageDB), compilerVersion)
 import Distribution.Verbosity(silent)
 
 import Control.Monad(liftM)
 
 configureGHC = configure silent Nothing Nothing defaultProgramConfiguration
+
+ghcVersion = liftM (compilerVersion . fst) configureGHC
 
 pkgIndex = do (_,conf) <- configureGHC
               getInstalledPackages
