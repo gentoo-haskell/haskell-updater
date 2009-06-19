ba5f16dda3d89a09ee038b3afcac2ab1739b094a
Ivan.Miljenovic <Ivan.Miljenovic@gmail.com>
2009-06-19 13:33:20 +0000

    Use explicit ExitCode stuff along with exitWith to ensure the program ends

--- Main.hs
+++ Main.hs
@@ -4,16 +4,23 @@ import Distribution.Gentoo.GHC
 import Distribution.Gentoo.Packages
 import Distribution.Gentoo.PkgManager
 
-buildPkgsFrom       :: IO [Package] -> PkgManager ->  IO ()
+import System.Exit(ExitCode(ExitSuccess), exitWith)
+
+success     :: String -> IO ExitCode
+success msg = do putStrLn msg
+                 exitWith ExitSuccess
+
+buildPkgsFrom       :: IO [Package] -> PkgManager ->  IO ExitCode
 buildPkgsFrom ps pm = do ps' <- ps
                          if null ps'
-                           then putStrLn "Nothing to build!"
-                           else buildPkgs pm ps' >> return ()
+                           then success "Nothing to build!"
+                           else buildPkgs pm ps' >>= exitWith
 
-ghcUpgrade    :: PkgManager -> IO ()
+ghcUpgrade    :: PkgManager -> IO ExitCode
 ghcUpgrade pm = do putStrLn "Looking for packages from old GHC installs..."
                    buildPkgsFrom rebuildPkgs pm
 
-ghcCheck    :: PkgManager -> IO ()
+ghcCheck    :: PkgManager -> IO ExitCode
 ghcCheck pm = do putStrLn "Looking for packages that need to rebuilt..."
                  buildPkgsFrom brokenPkgs pm
+
