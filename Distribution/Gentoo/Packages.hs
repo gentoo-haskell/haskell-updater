f4e4db34d9583de1c6990feb2fc8572a8f6b0564
Ivan.Miljenovic <Ivan.Miljenovic@gmail.com>
2009-10-23 11:16:31 +0000

    Add some documentation, and a few code neatenings.

--- Distribution/Gentoo/Packages.hs
+++ Distribution/Gentoo/Packages.hs
@@ -24,7 +24,7 @@ import Distribution.Gentoo.Util
 
 import Data.Char(isDigit, isAlphaNum)
 import Data.List(delete)
-import Data.Maybe(catMaybes, listToMaybe)
+import Data.Maybe(mapMaybe, listToMaybe)
 import qualified Data.ByteString.Char8 as BS
 import Data.ByteString.Char8(ByteString)
 import System.Directory( doesDirectoryExist
@@ -34,18 +34,22 @@ import Control.Monad(filterM, liftM)
 
 -- -----------------------------------------------------------------------------
 
---- Type defns, aliases, etc.
+-- Representation of a cat/pkgname in Gentoo.  Note that this is
+-- overly simplified.
 
 type Category = String
-type Pkg = String
-type VerPkg = String
+type Pkg = String -- Package name.
+type VerPkg = String -- Package name with version.
 type VCatPkg = (Category, VerPkg)
 type Slot = String
 
+-- When we are (re-)building packages, we don't care about the
+-- version, just the slot.
 data Package = Package Category Pkg (Maybe Slot)
              deriving(Eq, Ord, Show, Read)
 
--- Equality, ignoring the Slot
+-- Package equality, ignoring the Slot (i.e. same category and package
+-- name).
 samePackageAs :: Package -> Package -> Bool
 samePackageAs (Package c1 p1 _) (Package c2 p2 _)
   = c1 == c2 && p1 == p2
@@ -56,6 +60,7 @@ ghcPkg = Package "dev-lang" "ghc" Nothing
 ghcBinPkg :: Package
 ghcBinPkg = Package "dev-lang" "ghc-bin" Nothing
 
+-- Return all packages that are not a version of GHC.
 notGHC :: [Package] -> [Package]
 notGHC = filter (\p -> isNot ghcPkg p && isNot ghcBinPkg p)
   where
@@ -68,11 +73,14 @@ printPkg (Package c p s) = addS cp
     addS = maybe id (flip (++) . (:) ':') s
     cp = c ++ '/' : p
 
+-- Determine which slot the specific version of the package is in and
+-- create the appropriate Package value.
 toPackage           :: VCatPkg -> IO Package
 toPackage cp@(c,vp) = do sl <- getSlot cp
                          let p = stripVersion vp
                          return $ Package c p sl
 
+-- Determine which slot the specific version of the package is in.
 getSlot    :: VCatPkg -> IO (Maybe Slot)
 getSlot cp = do ex <- doesFileExist sFile
                 if ex
@@ -80,11 +88,11 @@ getSlot cp = do ex <- doesFileExist sFile
                   else return Nothing
   where
     sFile = pkgPath cp </> "SLOT"
-
     parse = do fl <- readFile sFile
                -- Don't want the trailing newline
                return $ listToMaybe $ lines fl
 
+-- | Remove the version information from the package name.
 stripVersion :: VerPkg -> Pkg
 stripVersion = concat . takeUntilVer . breakAll partSep
   where
@@ -99,7 +107,7 @@ stripVersion = concat . takeUntilVer . breakAll partSep
     isVerFront ('-':as) = all (\a -> isDigit a || a == '.') as
     isVerFront _        = False
 
-pkgPath :: VCatPkg -> FilePath
+pkgPath        :: VCatPkg -> FilePath
 pkgPath (c,vp) = pkgDBDir </> c </> vp
 
 pkgDBDir :: FilePath
@@ -107,11 +115,12 @@ pkgDBDir = "/var/db/pkg"
 
 -- -----------------------------------------------------------------------------
 
--- Contents
+-- Parsing the CONTENTS file of installed packages.
 
-data Content =   Dir BSFilePath
-               | Obj BSFilePath
-                 deriving (Eq, Show)
+-- Representation of individual lines in a CONTENTS file.
+data Content = Dir BSFilePath
+             | Obj BSFilePath
+               deriving (Eq, Show)
 
 isDir         :: Content -> Bool
 isDir (Dir _) = True
@@ -125,7 +134,7 @@ pathOf           :: Content -> BSFilePath
 pathOf (Dir dir) = dir
 pathOf (Obj obj) = obj
 
--- Searching predicates
+-- Searching predicates.
 
 hasContentMatching   :: (BSFilePath -> Bool) -> [Content] -> Bool
 hasContentMatching p = any p . map pathOf
@@ -136,9 +145,7 @@ hasDirMatching p = hasContentMatching p . filter isDir
 hasObjMatching   :: (BSFilePath -> Bool) -> [Content] -> Bool
 hasObjMatching p = hasContentMatching p . filter isObj
 
--- Parse the CONTENTS file
-
-
+-- Parse the CONTENTS file.
 parseContents    :: VCatPkg -> IO [Content]
 parseContents cp = do ex <- doesFileExist cFile
                       if ex
@@ -148,7 +155,7 @@ parseContents cp = do ex <- doesFileExist cFile
     cFile = pkgPath cp </> "CONTENTS"
 
     parse = do lns <- liftM BS.lines $ BS.readFile cFile
-               return $ catMaybes $ map (parseCLine .BS.words) lns
+               return $ mapMaybe (parseCLine .BS.words) lns
 
     -- Use unwords of list rather than taking next element because of
     -- how spaces are represented in file names.
@@ -167,22 +174,26 @@ parseContents cp = do ex <- doesFileExist cFile
     obj = BS.pack "obj"
     dir = BS.pack "dir"
 
-
 -- -----------------------------------------------------------------------------
 
 -- Find the package (if any) that contain this file.
--- Assumes collision protection (i.e. at most one package per file)
+-- Assumes collision protection (i.e. at most one package per file).
 hasFile    :: FilePath -> IO (Maybe Package)
 hasFile fp = liftM listToMaybe $ pkgsHaveContent p
   where
     fp' = BS.pack fp
     p = hasObjMatching ((==) fp')
 
+-- Find which packages have Content information that matches the
+-- provided predicate; to be used with the searching predicates
+-- above.
 pkgsHaveContent   :: ([Content] -> Bool) -> IO [Package]
 pkgsHaveContent p = do cs <- installedCats'
                        cps <- concatMapM (catHasContent p) cs
                        mapM toPackage cps
 
+-- Determine if this is a valid Category (such that at least one
+-- package in that category has been installed).
 isCat    :: String -> IO Bool
 isCat fp = do isD <- doesDirectoryExist (pkgDBDir </> fp)
               return $ isD && isCat' fp
@@ -191,6 +202,7 @@ isCat fp = do isD <- doesDirectoryExist (pkgDBDir </> fp)
     isCat' "world" = False
     isCat' _       = True
 
+-- Return all Categories known in this system.
 installedCats :: IO [Category]
 installedCats = filterM isCat =<< getDirectoryContents' pkgDBDir
 
@@ -198,15 +210,15 @@ installedCats = filterM isCat =<< getDirectoryContents' pkgDBDir
 -- first in our list.
 installedCats' :: IO [Category]
 installedCats' = do cats <- installedCats
-                    let cats' = if haskCat `elem` cats
-                                then haskCat : delete haskCat cats
-                                else cats
-                    return cats'
+                    return $ if haskCat `elem` cats
+                             then haskCat : delete haskCat cats
+                             else cats
   where
     haskCat = "dev-haskell"
 
-catHasContent     :: ([Content] -> Bool) -> Category -> IO
-                     [VCatPkg]
+-- Return all packages in this category whose contents match the
+-- provided predicate.
+catHasContent     :: ([Content] -> Bool) -> Category -> IO [VCatPkg]
 catHasContent p c = do inDir <- getDirectoryContents' cfp
                        let psbl = map ((,) c) inDir
                        pkgs  <- filterM (doesDirectoryExist . pkgPath) psbl
@@ -214,5 +226,6 @@ catHasContent p c = do inDir <- getDirectoryContents' cfp
   where
     cfp = pkgDBDir </> c
 
+-- Determine if this package matches the predicate.
 hasContent   :: ([Content] -> Bool) -> VCatPkg -> IO Bool
 hasContent p = liftM p . parseContents
