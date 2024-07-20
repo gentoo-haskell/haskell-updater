eefa651ca754255cb41b7b9fec2a4c7e18fbcb02
hololeap <hololeap@protonmail.com>
2024-07-20 17:50:50 -0600

    Util: Add NESet data type
    A Set that can never be empty
    
    Signed-off-by: hololeap <hololeap@protonmail.com>

--- Distribution/Gentoo/Util.hs
+++ Distribution/Gentoo/Util.hs
@@ -15,12 +15,18 @@ module Distribution.Gentoo.Util
        , breakAll
        , These(..)
        , these
+       , NESet(..)
+       , singletonNE
+       , insertNE
+       , memberNE
+       , toListNE
        ) where
 
 import Data.Bifoldable
 import Data.Bifunctor
 import Data.Bitraversable
 import qualified Data.List as L
+import qualified Data.Set as S
 import Data.ByteString.Char8(ByteString)
 
 -- Alias used to indicate that this ByteString represents a FilePath
@@ -57,3 +63,24 @@ these :: (a -> b -> c) -> (a -> c) -> (b -> c) -> These a b -> c
 these f _ _ (These a b) = f a b
 these _ g _ (This a) = g a
 these _ _ h (That b) = h b
+
+data NESet a = a :~ S.Set a
+    deriving (Show, Eq, Ord, Foldable)
+
+instance Ord a => Semigroup (NESet a) where
+    (x1 :~ s1) <> (x2 :~ s2) =
+        x1 :~ (if x1 == x2 then id else (S.insert x2)) (s1 <> s2)
+
+singletonNE :: a -> NESet a
+singletonNE x = x :~ S.empty
+
+insertNE :: Ord a => a -> NESet a -> NESet a
+insertNE x n@(y :~ s)
+    | x == y = n
+    | otherwise = y :~ S.insert x s
+
+memberNE :: Ord a => a -> NESet a -> Bool
+memberNE x (y :~ s) = x == y || S.member x s
+
+toListNE :: Ord a => NESet a -> [a]
+toListNE (x :~ s) = S.toList (S.insert x s)
